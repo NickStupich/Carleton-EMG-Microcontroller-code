f8c1009ce9b3c9a1e210bc7d59b292985123b42c
Nick Stupcih <nick.stupich@gmail.com>
2012-02-22 18:06:51 -0500

    modified clock outputs to run at 6kHz and 12kHz for the new switched cap notch and low pass filters

--- Native/i2sClock.h
+++ Native/i2sClock.h
@@ -3,6 +3,13 @@ PCONP |= (1<<27);//I2S power enabled - turned on above
 
 PINSEL4 |= (0x3F << 22);	//select clock pin function
 I2S_DAO = 0x0;				//
-I2S_TXRATE = 913;	//counter limit for the I2S.  This should be 899 in theory, but setting it to 913 will make the notch 
+
+//for 12kHz signal 
+PCLKSEL1 |= 3 << 22;
+I2S_TXRATE = 748;
+
+//for 20kHz signal
+//I2S_TXRATE = 913;	
+//counter limit for the I2S.  This should be 899 in theory, but setting it to 913 will make the notch 
 //centered at exactly 120Hz
 	
